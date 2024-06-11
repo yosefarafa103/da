c80c5c16d3b6ed56daa6eadb68463575bb596030
yosef <jooyosef173@gmail.com>
2024-06-11 22:18:59 +0300

    1

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
+"use client";
+import gsap from "gsap";
+import { useEffect } from "react";
+import { useGSAP } from "@gsap/react";
 const About = () => {
-  return <div>About</div>;
+  useGSAP(() => {
+    gsap.from(".text", {
+      y: 100,
+      stagger: 0.09,
+      duration: 1,
+    });
+  });
+  return (
+    <>
+      <div className="h-[50vh] px-[3.2vw] text-[22px] md:text-[3vw] flex-col text-white font-[helveticaNeue] flex items-center text-center justify-center bg-[#f55048]">
+        <section className="w-[70%]">
+          <div className="overflow-hidden">
+            <div className="relative text">
+              خلقنا لننطلق بجانبك من خلال تصميم العلامة التجارية وبنائها
+            </div>
+          </div>
+          <div className="overflow-hidden">
+            <div className="relative text">
+              واداره صفحات السوشيال ميديا وخدماتها وتصاميمها, لنكون الحليف
+              المثالي
+            </div>
+          </div>
+          <div className="overflow-hidden">
+            <div className="relative text">للتحول الرقمي الذي تطمح الية</div>
+          </div>
+        </section>
+      </div>
+      <div className="h-[50vh] text-white px-[2vw] pt-[1.5vw] text-right font-[helveticaNeue] bg-[#000]">
+        <h3 className="md:text-[2.5vw] text-[26px] font-bold">
+          {" "}
+          نبذه حول ”دبـــــــــــــــــــــــــــــــــــوس{" "}
+        </h3>
+        <p className="text-[#f7f7f7] mt-[20px] text-[18px] w-[300px] ml-auto">
+          فريق سعودي, وخبرات سعوديه متمكنة وقوية, "
+          <span>ويا عز مين قصد ربعه</span>", همك همنا وطموحك هو طموحنا, انت
+          سلمنا الفكرة وطريقة اخراج مشروعك علينا
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default About;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,48 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import logo from "@/assets/imgs/Dabous-01.png";
-import React from "react";
-import ToggleBars from "./ToggleBars";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import F from "../hooks/toggleHeader.js";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 const Header = () => {
+  const [a, setA] = F();
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    if (!a) {
+      tl.to(".nav-bar", {
+        height: 230,
+        duration: 1,
+      });
+      tl.from(".lnk", {
+        x: 50,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.05,
+      });
+      // lnk
+    } else {
+      tl.to(".nav-bar", {
+        height: 0,
+        duration: 1,
+      });
+      tl.from(".lnk", {
+        x: 0,
+        opacity: 0,
+        duration: 1,
+        stagger: 0.05,
+      });
+    }
+  }, [a]);
   const linksEn = ["/", "/about-us", "/our-services", "contact-us"];
+  console.log(a);
   return (
     <header
       style={{ direction: "rtl" }}
-      className="flex items-center justify-between bg-[#000] px-[22px] md:px-[2.1vw]"
+      className="flex relative items-center justify-between bg-[#000] px-[22px] md:px-[2.1vw]"
     >
       <Link href={`/`} className="block w-fit overflow-hidden">
         <Image
@@ -29,12 +63,43 @@ const Header = () => {
       </section>
       <section className="text-white flex gap-[15px] items-center">
         <Link href={`/en`} className="font-[montserrat] text-[22px] font-bold">
-          <span className='font-[helveticaNeue]'>عربي</span> / EN
+          <span className="font-[helveticaNeue]">عربي</span> / EN
         </Link>
-        <section>
-          <ToggleBars />
+        <section className="block md:hidden">
+          <div
+            className="text-[20px] cursor-pointer"
+            onClick={() => setA((prev) => !prev)}
+          >
+            {a ? (
+              <span>
+                <FaBars />
+              </span>
+            ) : (
+              <span>
+                <IoClose />
+              </span>
+            )}
+          </div>
         </section>
       </section>
+      {/* Header Mobile */}
+      {!a && (
+        <section className="nav-bar origin-top absolute  border-[1px] border-[#bbb] border-solid  md:hidden  h-0 bg-black z-[1000] w-[99.5%] top-full left-1/2 -translate-x-1/2">
+          <div className="flex py-[15px] gap-[15px] flex-col pr-[12vw]">
+            {["الرئيسية", "من نحن", "خدماتنا", "اتصل بنا"].map((l, i) => (
+              <div key={l} className="lnk" onClick={() => setA((p) => !p)}>
+                <Link
+                  className="text-white hover:text-[#808080] transition-all duration-300 font-bold text-[20px]"
+                  href={linksEn[i]}
+                >
+                  {l}
+                </Link>
+              </div>
+            ))}
+          </div>
+        </section>
+      )}
+      {/* Header Mobile */}
     </header>
   );
 };
